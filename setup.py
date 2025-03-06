328d32c4f70448cf63f93c5282121e3cb0fc77af
anuragsinghbhandari <anuoo3ups@gmail.com>
2025-03-06 14:33:36 +0530

    version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='StaticScrape',
-    version='0.2',
+    version='0.2.1',
     description="A command-line tool to clone any website's front end",
     long_description=long_description,  # This ensures the README appears on PyPI
     long_description_content_type="text/markdown",  # Specify markdown format
